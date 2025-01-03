83d292becf2238bd27889e7ff83f3639e80807e4
sameershaiks <167987805+sameershaiks@users.noreply.github.com>
2025-01-03 16:47:06 +0000

    sameer js added

---
+++ courses/javascript/task/sameer.js
@@ -0,0 +1,12 @@
+var firstname = `shaik`;
+var lastname  =`sameer`;
+
+var location  = `hyderabad`;
+var fullname  =  "{shaik  sameer}"
+
+
+ var skills   = "html, css, javascript"
+
+
+ console.log (fullname);
+ console.log (skills);
